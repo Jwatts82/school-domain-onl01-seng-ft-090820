6ac2e5dbdbab8472dc9811466b167e2d87821fe4
Jessica Watts <jessperr182@hotmail.com>
2020-09-15 22:07:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,28 @@
 # code here!
 class School
   attr_accessor :roster
-  attr_reader :name 
-  
-  def initialize(name)
-    @name = name
-  end
-  
-  
-  
-  
-  
-  
-end
+  attr_reader :name
 
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student (student, grade)
+
+    if @roster.keys.include?(grade)
+      @roster[grade].push(student)
+      @roster[grade]
+    else
+    @roster[grade] = [student]
+    end
+  end
+
+  def grade (grade)
+   return  @roster[grade]
+  end
+
+  def sort
+  @roster.each {|grade, students| students.sort!}
+  end
+end
