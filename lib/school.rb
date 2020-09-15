7ebcce9118608cc77e71690730ecc77ab8dc411b
Jessica Watts <jessperr182@hotmail.com>
2020-09-15 22:02:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,11 @@
 # code here!
 class School
-  def add_student(student_name, grade)
-  if @roster[grade]
-    @roster[grade] << student_name
-  else
-    @roster[grade] = []
-    @roster[grade] << student_name
-  end
+  
+  
+  
+  
+  
+  
+  
 end
 
